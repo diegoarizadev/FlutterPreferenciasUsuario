e465ca4fca8da45f0af3a1f88939bcb466c9ec1b
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-06 11:43:52 -0500

    Se agregan loas primeras parge, así como un nuevo estilo de navegación.

---
+++ preferenciasusuario/lib/src/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  static final String routeName =
+      'Home'; //Referecia del routeName para obtener la pagina
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Preferencias de usuario'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Color secundario:'),
+          Divider(),
+          Text('Genero:'),
+          Divider(),
+          Text('Nombre de usuario:'),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
